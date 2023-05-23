73ff85ea4ca87f1fccc01ef69686f06429f6d552
jingjingya <240538502@qq.com>
2023-05-24 00:48:40 +0800

    抽奖小demo

--- Lottery_system/app.py
+++ Lottery_system/app.py
@@ -1,20 +1,27 @@
-# 让我们的电脑可以支持服务访问
-# 需要一个web 框架
-# pip install Flask  在命令行里面
-
 from flask import Flask, render_template
+from random import randint
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 heros = ['黑暗之女', "狂战士", "正义巨像", "卡牌大师", "德邦总管", "无畏战车", "诡术妖姬", "猩红收割者", "远古恐惧",
          "正义使者", "无极剑圣", "牛头酋长", "符文法师", "亡灵战神", "战争女神", "众星之子"]
 
 
-@app.route("/hello")
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/index')
 def index():
-    return render_template('index.html', heros=heros)
+    return render_template("index.html", heros=heros)
 
 
-if __name__ == "__main__":
-    print(app.url_map)
+@app.route('/choujiang')
+def choujiang():
+    num = randint(0, len(heros))
+    return render_template("index.html", heros=heros, hero=heros[num])
+
+
+if __name__ == '__main__':
     app.run(debug=True)
