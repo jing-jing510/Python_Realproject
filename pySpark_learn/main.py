361e1f68f44787fc84047c5972460849818878b3
jingjing <17601638013@163.com>
2023-05-26 16:33:36 +0800

    pySpark_未完成

---
+++ pySpark_learn/main.py
@@ -0,0 +1,25 @@
+# auth ： xiaokou
+# date ： 2023/5/26 16:23
+
+from pyspark import SparkConf,SparkContext
+
+conf = SparkConf().setMaster("local[*]").setAppName("test_spark_app")
+
+sc = SparkContext(conf=conf)
+
+# print(sc.version)
+rdd1 = sc.parallelize([1,2,3,4,5])
+rdd2 = sc.parallelize((1,2,3,4,5))
+rdd3 = sc.parallelize("12345")
+rdd4 = sc.parallelize({1,2,3,4,5})
+rdd5 = sc.parallelize({"key1":"1","key2":"2"})
+
+print(rdd1.collect())
+print(rdd2.collect())
+print(rdd3.collect())
+print(rdd4.collect())
+print(rdd5.collect())
+
+
+
+sc.stop()
