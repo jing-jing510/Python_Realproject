95ae649a652e30e0055389a859c1194461280a40
jingjing <17601638013@163.com>
2023-05-26 17:33:05 +0800

    pySpark_未完成

--- pySpark_learn/main.py
+++ pySpark_learn/main.py
@@ -8,18 +8,18 @@ conf = SparkConf().setMaster("local[*]").setAppName("test_spark_app")
 sc = SparkContext(conf=conf)
 
 # print(sc.version)
-rdd1 = sc.parallelize([1,2,3,4,5])
-rdd2 = sc.parallelize((1,2,3,4,5))
-rdd3 = sc.parallelize("12345")
-rdd4 = sc.parallelize({1,2,3,4,5})
-rdd5 = sc.parallelize({"key1":"1","key2":"2"})
+# rdd1 = sc.parallelize([1,2,3,4,5])
+# rdd2 = sc.parallelize((1,2,3,4,5))
+# rdd3 = sc.parallelize("12345")
+# rdd4 = sc.parallelize({1,2,3,4,5})
+# rdd5 = sc.parallelize({"key1":"1","key2":"2"})
+#
+# print(rdd1.collect())
+# print(rdd2.collect())
+# print(rdd3.collect())
+# print(rdd4.collect())
+# print(rdd5.collect())
 
-print(rdd1.collect())
-print(rdd2.collect())
-print(rdd3.collect())
-print(rdd4.collect())
-print(rdd5.collect())
-
-
+sc.textFile()
 
 sc.stop()
